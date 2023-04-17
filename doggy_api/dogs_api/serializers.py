480bad2cccfa7b9f0cfdf6f20825ca2597a37597
“Jeremy <jeremy.kolker@gmail.com>
2023-04-16 20:58:39 -0400

    updated models to include photo

--- doggy_api/dogs_api/serializers.py
+++ doggy_api/dogs_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Dog
 class DogSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Dog # tell django which model to use
-        fields = ('id', 'name', 'age', 'breed', 'walk_time', 'feeding_instructions') # tell django which fields to include
+        fields = ('id', 'name', 'age', 'breed', 'walk_time', 'feeding_instructions', 'photo_url') # tell django which fields to include
